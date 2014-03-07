35fb6cc35b6a91257f8dba6aa61cff2d99aa1e58
jcollyer <collyerdesign@gmail.com>
2014-03-06 19:19:50 -0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Scaphon::Application.config.secret_key_base = '1d819bbfd06c10216482a867a78bc47f0ae70235dad6cbe6261036ee7de75240c2de30db2c44b6c5e4652dc829fdf0f82ee9240680d5ffe6a86e8fbf4aaa4ba2'
