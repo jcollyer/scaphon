eaf4aa50f680740d8133d4ca6c0d643737e1bd34
jcollyer <collyerdesign@gmail.com>
2014-03-19 19:20:44 -0700

    add attachment migration and add attachment file

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140319024512) do
+ActiveRecord::Schema.define(version: 20140319033633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "attachments", force: true do |t|
+    t.string   "name"
+    t.integer  "fileable_id"
+    t.string   "fileable_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "conferences", force: true do |t|
     t.string   "name"
