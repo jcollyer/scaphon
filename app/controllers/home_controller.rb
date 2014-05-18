b184c944f302f16d0d61e6f9681e49eb8e7e52a6
jcollyer <collyerdesign@gmail.com>
2014-05-17 21:02:19 -0700

    add images for scholarships and newsletter on home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,12 @@
 class HomeController < ApplicationController
   def index
     @event = Affair.last
-    @picture = @event.pictures.first
+    @event_picture = @event.pictures.first
+
+    @scholarship = Scholarship.last
+    @scholarship_picture = @scholarship.pictures.first
+
+    @newsletter = Newsletter.last
+    @newsletter_picture = @newsletter.pictures.first
   end
 end
