987c21d460de1f8c0b8eb883c6a0efba4e9c6b69
jcollyer <collyerdesign@gmail.com>
2014-05-18 12:32:54 -0700

    add rollover to homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,13 @@
 class HomeController < ApplicationController
   def index
+    @events = Affair.all.limit(2)
     @event = Affair.last
     @event_picture = @event.pictures.first
 
+    @newsletters = Newsletter.all.limit(3)
     @newsletter = Newsletter.last
     @newsletter_picture = @newsletter.pictures.first
+
+    @scholarships = Scholarship.all.limit(3)
   end
 end
