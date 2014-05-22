5454009990bdea2f8b2cc739d714e37a21256bd2
jcollyer <collyerdesign@gmail.com>
2014-05-22 08:02:01 -0700

    update home display events

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @events = Affair.all.limit(2)
+    @events = Affair.all.limit(3)
     @event = Affair.last
     @event_picture = @event.pictures.first
 
