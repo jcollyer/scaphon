feea5781cdf67bdfd9094292cd4fee26a0af36d8
jcollyer <collyerdesign@gmail.com>
2014-09-30 07:55:57 -0700

    fix attachment model, to work with pdf and doc

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -2,6 +2,7 @@ class Attachment < ActiveRecord::Base
   belongs_to :fileable, :polymorphic => true
 
   has_attached_file :file,
+    # :styles => { :file_thumbnail => ["", :jpg] },
     :storage => :s3,
     :bucket => 'scaphon_data',
     :s3_credentials => {
@@ -10,4 +11,15 @@ class Attachment < ActiveRecord::Base
     },
     :path => ":rails_root/public/system/:attachment/:id/:style/:filename",
     :url => "/system/:attachment/:id/:style/:filename"
+
+
+    # validates_attachment :file, content_type: { content_type: "docx" }
+    # validates_attachment_content_type :file, :content_type => %w(application/pdf application/doc)
+    # validates_format_of :file_file_name, :with => %r{\.(docx|doc|pdf)$}i
+    # validates_attachment_content_type :file, :content_type => 'text/plain'
+
+
+
+    validates_attachment_content_type :file, :content_type => %w(application/msword application/vnd.ms-office application/vnd.ms-excel application/vnd.openxmlformats-officedocument.spreadsheetml.sheet application/pdf)
+
 end
