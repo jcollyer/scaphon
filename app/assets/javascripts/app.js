96ab7383dc5dc2abacb7429c777ddcb15c415004
jcollyer <collyerdesign@gmail.com>
2014-10-25 12:46:51 -0700

    add sticky header, footer, with js and css

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -4,5 +4,15 @@ $(function(){
   var ppcode = $('#new_pp_code');
   var code = paypal.text();
   ppcode.append(code);
+
+
+  var footerHeight = document.getElementById("footer").offsetHeight;
+  var navHeight = document.getElementById("nav").offsetHeight;
+  var topBarHeight = document.getElementById("top-bar").offsetHeight;
+  var content = document.getElementById("content");
+
+  content.style.marginBottom = footerHeight + "px";
+  content.style.marginTop = navHeight + topBarHeight + "px";
+  // document.getElementById("footer-buffer").style.paddingBottom = height + 'px';
 });
 
