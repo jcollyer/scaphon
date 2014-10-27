5bcc7503b3741968bab9bdd94bcc5cd4b81b568c
jcollyer <collyerdesign@gmail.com>
2014-10-27 07:52:18 -0700

    update js to be ready always, now works with turbo links

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -16,3 +16,29 @@ $(function(){
   // document.getElementById("footer-buffer").style.paddingBottom = height + 'px';
 });
 
+var ready;
+ready = function() {
+
+  // ...your javascript goes here...
+
+
+  // paypal - ensure document is ready
+  var paypal = $('#initial_paypal_code');
+  var ppcode = $('#new_pp_code');
+  var code = paypal.text();
+  ppcode.append(code);
+
+
+  // fixed elements setup
+  var footerHeight = document.getElementById("footer").offsetHeight;
+  var navHeight = document.getElementById("nav").offsetHeight;
+  var topBarHeight = document.getElementById("top-bar").offsetHeight;
+  var content = document.getElementById("content");
+
+  content.style.marginBottom = footerHeight + "px";
+  content.style.marginTop = navHeight + topBarHeight + "px";
+
+};
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
