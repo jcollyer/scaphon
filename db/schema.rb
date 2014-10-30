a754b730cc17bebe38bd98650b863f166b6c25e7
jcollyer <collyerdesign@gmail.com>
2014-10-30 08:09:10 -0700

    remove address from users, using street_address

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141030150111) do
+ActiveRecord::Schema.define(version: 20141030150441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -129,7 +129,6 @@ ActiveRecord::Schema.define(version: 20141030150111) do
     t.string   "password_salt"
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "address"
     t.string   "city"
     t.string   "state"
     t.integer  "zip"
