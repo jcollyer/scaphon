4c21b616be1f5eb0c87ab47e67204afdaac71e56
jcollyer <collyerdesign@gmail.com>
2014-10-29 19:56:35 -0700

    remove unneeded js

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,21 +1,3 @@
-$(function(){
-  //paypal - ensure document is ready
-  var paypal = $('#initial_paypal_code');
-  var ppcode = $('#new_pp_code');
-  var code = paypal.text();
-  ppcode.append(code);
-
-
-  var footerHeight = document.getElementById("footer").offsetHeight;
-  var navHeight = document.getElementById("nav").offsetHeight;
-  var topBarHeight = document.getElementById("top-bar").offsetHeight;
-  var content = document.getElementById("content");
-
-  content.style.marginBottom = footerHeight + "px";
-  content.style.marginTop = navHeight + topBarHeight + "px";
-  // document.getElementById("footer-buffer").style.paddingBottom = height + 'px';
-});
-
 var ready;
 ready = function() {
 
