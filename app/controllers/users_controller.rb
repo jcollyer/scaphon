b47db1838bf37419062db2d7076c56157553821d
jcollyer <collyerdesign@gmail.com>
2014-11-02 07:45:35 -0800

    show, edit actions w params for User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,17 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    # binding.pry
   end
 
   def create
@@ -22,7 +31,19 @@ class UsersController < ApplicationController
     end
   end
 
-  private
+  def update
+    @user = User.find(params[:id])
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   def destroy
     @user = User.find(params[:id])
     @user.destroy
@@ -32,7 +53,15 @@ class UsersController < ApplicationController
     end
   end
 
+  private
+    def set_supporter
+      @user = User.find(params[:id])
+    end
     def user_params
-      params.require(:user).permit(:email, :password_hash, :password)
+      params.require(:user).permit(:email, :password_hash, :password, :first_name, :last_name,
+                                   :city, :state, :zip, :phone_home, :phone_work, :nurse,
+                                   :institution, :rn_id, :stage_registerd, :aphon_member,
+                                   :scaphon_member, :street_address, :aphon_member_number,
+                                   :aphon_expiration_date)
     end
 end
