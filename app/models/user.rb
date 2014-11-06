cf2d47aced879301eeec0b6b51926e546ebee0c0
jcollyer <collyerdesign@gmail.com>
2014-11-06 07:46:32 -0800

    use cookies for sessions

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,11 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 2 }
   validates :email, uniqueness: true
 
+  before_create { generate_token(:auth_token) }
+
+  def generate_token(column)
+    begin
+      self[column] = SecureRandom.urlsafe_base64
+    end while User.exists?(column => self[column])
+  end
 end
