ebd09781fc8a2efbad17cfe3e9032a15460701d2
jcollyer <collyerdesign@gmail.com>
2014-11-09 09:01:16 -0800

    mailer for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,25 @@ Scaphon::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = { :host => "www.scaphon.org" }
+
+  # config.action_mailer.delivery_method = :sendmail
+  # Defaults to:
+  # config.action_mailer.sendmail_settings = {
+  #   location: '/usr/sbin/sendmail',
+  #   arguments: '-i -t'
+  # }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.default_options = {from: 'no-reply@example.com'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            'collyerdesign@gmail.com',
+    password:             'jcollyer5849',
+    authentication:       'plain',
+    enable_starttls_auto: true  }
 end
