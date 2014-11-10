1aeeac0e02723f445b568ca73f9e717749e7d0ad
jcollyer <collyerdesign@gmail.com>
2014-11-10 07:18:27 -0800

    update mail in environments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,22 +80,25 @@ Scaphon::Application.configure do
 
   config.action_mailer.default_url_options = { :host => "scaphon-rails4.herokuapp.com" }
 
-  # config.action_mailer.delivery_method = :sendmail
-  # Defaults to:
-  # config.action_mailer.sendmail_settings = {
-  #   location: '/usr/sbin/sendmail',
-  #   arguments: '-i -t'
-  # }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   :address        => 'smtp.sendgrid.net',
+  #   :port           => '25',
+  #   :authentication => :plain,
+  #   :user_name      => ENV['app30095341@heroku.com'],
+  #   :password       => ENV['ajgnqqoj'],
+  #   :domain         => 'scaphon-rails4.herokuapp.com',
+  #   :enable_starttls_auto => true
+  # }
+
   config.action_mailer.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '25',
-    :authentication => :plain,
-    :user_name      => ENV['app30095341@heroku.com'],
-    :password       => ENV['ajgnqqoj'],
-    :domain         => 'scaphon-rails4.herokuapp.com',
-    :enable_starttls_auto => true
-  }
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            'collyerdesign@gmail.com',
+    password:             'jcollyer5849',
+    authentication:       'plain',
+    enable_starttls_auto: true  }
 end
