b4f7e2fdca10bd5e145392e20f5f474967d58a81
jcollyer <collyerdesign@gmail.com>
2014-11-10 07:11:29 -0800

    update prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Scaphon::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { :host => "www.scaphon.org" }
+  config.action_mailer.default_url_options = { :host => "scaphon-rails4.herokuapp.com" }
 
   # config.action_mailer.delivery_method = :sendmail
   # Defaults to:
@@ -88,7 +88,6 @@ Scaphon::Application.configure do
   # }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  # config.action_mailer.default_options = {from: 'no-reply@example.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
