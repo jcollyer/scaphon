253f779bc0611e76824a194ba92c2f0db0f6e7b9
jcollyer <collyerdesign@gmail.com>
2014-11-16 08:30:52 -0800

    ADD SORT

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   # before_filter :authorize
+  helper_method :sort_column, :sort_direction
   def index
-    @users = User.all
+    @users = User.order(sort_column + " " + sort_direction)
   end
 
   def show
@@ -58,5 +59,13 @@ class UsersController < ApplicationController
                                    :institution, :rn_id, :state_registerd, :aphon_member,
                                    :street_address, :aphon_member_number,
                                    :aphon_expiration_date, :password_reset_sent_at)
+    end
+
+    def sort_column
+      User.column_names.include?(params[:sort]) ? params[:sort] : "email"
+    end
+
+    def sort_direction
+      %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
     end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,15 @@ module ApplicationHelper
     @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, space_after_headers: true, fenced_code_blocks: true, hard_wrap: true)
     @markdown.render(content).html_safe
   end
+
+  def sortable(column, title = nil)
+    title ||= column.titleize
+    direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+    if column == sort_column
+      css_class = sort_direction == "asc" ? "current fa fa-chevron-up" : "current fa fa-chevron-down"
+    else
+      css_class = nil
+    end
+    link_to title, {:sort => column, :direction => direction}, {:class => css_class}
+  end
 end
