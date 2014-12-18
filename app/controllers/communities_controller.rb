4ce30e76487f63fa26c36c832187baacbfb006d1
jcollyer <collyerdesign@gmail.com>
2014-12-17 18:33:20 -0800

    restrict community

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -1,4 +1,5 @@
 class CommunitiesController < ApplicationController
+  before_filter :authorize, except: [:index]
   before_action :set_community, only: [:show, :edit, :update, :destroy]
 
   # GET /communities
