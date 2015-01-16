20e86702f6aadbb649b64d5f26584c627d1cd648
jcollyer <collyerdesign@gmail.com>
2015-01-15 18:54:24 -0800

    update mailer in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Scaphon::Application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
@@ -78,18 +78,18 @@ Scaphon::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { :host => "scaphon.org" }
+  config.action_mailer.default_url_options = { :host => "www.scaphon.org" }
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
-    :port           => '25',
+    :port           => '587',
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'scaphon.org',
+    :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
 end
